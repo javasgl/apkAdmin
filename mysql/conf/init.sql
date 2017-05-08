0e81d3609af821af4f600aaa8d4e42862f72a20b
ganglin.song <ganglin.song@yourdream.cc>
2017-05-08 22:31:25 +0800

    feat: update

--- mysql/conf/init.sql
+++ mysql/conf/init.sql
@@ -1,10 +1,12 @@
 DROP USER IF EXISTS 'apkAdmin'; 
 CREATE USER 'apkAdmin'@'%'; 
 CREATE DATABASE IF NOT EXISTS apkAdmin; 
+GRANT ALL ON apkAdmin.* TO 'apkAdmin'@'%' IDENTIFIED BY '123456';
+
+## create tables apk_channels
 USE apkAdmin;
 CREATE TABLE `apk_channels`(
 	`channelId` int unsigned not null default 0,
 	`channel` varchar(50) not null default '',
 	PRIMARY KEY(`channelId`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
-GRANT ALL ON apkAdmin.* TO 'apkAdmin'@'%' IDENTIFIED BY '123456';
