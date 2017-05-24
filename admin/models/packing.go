9c6e5c04ca8cc7de0ac27acc907fb86a33e9208b
ganglin.song <ganglin.song@yourdream.cc>
2017-05-24 18:58:20 +0800

    feat: add job

--- admin/models/packing.go
+++ admin/models/packing.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"strings"
+	"time"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 )
@@ -9,7 +12,8 @@ type PackingJobs struct {
 	JobId           int      `json:"-" orm:"PK"`
 	ApkVersion      string   `json:"apkVersion" orm:"size(20)"`
 	CheckedChannels []string `json:"checkedChannels" orm:"-"`
-	CreateTime      int
+	ApkChannel      string   `json:"-"`
+	CreateTime      int64
 	CreatorId       int
 	Status          int
 	DownloadUrl     string `orm:"size(100)"`
@@ -17,6 +21,10 @@ type PackingJobs struct {
 
 func AddPackingJob(packJob PackingJobs) bool {
 	//todo
+
+	packJob.CreateTime = time.Now().Unix()
+	packJob.ApkChannel = strings.Join(packJob.CheckedChannels, ",")
+
 	beego.Debug(packJob)
 
 	orm.NewOrm().Insert(&packJob)
